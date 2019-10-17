9cc9c6bb3d30d83912f18e977cee57b480eb4227
MateoGuerrero99 <56090119+MateoGuerrero99@users.noreply.github.com>
2019-10-17 15:35:35 -0500

    Add files via upload

---
+++ serie_numeros_while.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,cont=1;
+    printf("Ingrese un numero: ");
+    scanf("%d",&n);
+    for(cont=1;cont<=n;cont++)
+    {
+    	printf("%d\n",cont);
+	}
+    return 0;
+}
+
